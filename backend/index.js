12aee27500d509a5340c08296e08f225f784cba0
shivamkalra123 <kalrashivam47@gmail.com>
2026-06-03 16:35:23 +0530

    cors issue finally fixed

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
-    'https://roster-app-povc.onrender.com'
+    'https://roster-app-povc.onrender.com',
+    'https://roster-app-amak.vercel.app'
 ];
 
 // Configure CORS properly
