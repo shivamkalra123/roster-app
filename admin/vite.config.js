0227d32b1338fd41470ffa616c92e12fc13af818
shivamkalra123 <kalrashivam47@gmail.com>
2026-06-03 16:43:51 +0530

    deploying admin now

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://roster-app-povc.onrender.com",
         changeOrigin: true,
       },
     },
